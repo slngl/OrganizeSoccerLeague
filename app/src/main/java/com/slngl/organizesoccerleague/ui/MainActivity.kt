cda6a892aa9ec4baaa94e8fce2c1c67623a98a57
SELINGUL <dselin26@gmail.com>
2021-05-06 13:14:02 +0300

    Initial commit

---
+++ app/src/main/java/com/slngl/organizesoccerleague/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.slngl.organizesoccerleague.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.slngl.organizesoccerleague.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
