320ed4c3e71f6424264807ecb7b88bb213177861
SELINGUL <dselin26@gmail.com>
2021-05-06 13:35:25 +0300

    di - Hilt integrated and statusBarColor changed

--- app/src/main/java/com/slngl/organizesoccerleague/ui/MainActivity.kt
+++ app/src/main/java/com/slngl/organizesoccerleague/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.slngl.organizesoccerleague.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.slngl.organizesoccerleague.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
